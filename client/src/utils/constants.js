d0dc7eab76f04949d983e21bfad368c0175de610
Bryan Sanchez <bryanipsanchez@gmail.com>
2022-09-04 11:44:47 +0800

    Configured dapp for deployment on goerli testnet

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
 import storageABI from "../contracts/Storage.json";
 
-export const contractAddress = "0xC94b6556DB2c1c48aa5F4F3148C3611dEb1e7874";
+export const contractAddress = "0xEc48D1BC3CD0dcEe63A010d35e5D7285bCD0bd77";
 export const contractABI = storageABI.abi;
--- truffle-config.js
+++ truffle-config.js
@@ -23,6 +23,9 @@
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
 
+require("dotenv").config();
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const { API_URL, MNEMONIC } = process.env;
 module.exports = {
   contracts_build_directory: "./client/src/contracts",
   /**
@@ -60,14 +63,11 @@ module.exports = {
     //
     // Useful for deploying to a public network.
     // Note: It's important to wrap the provider as a function to ensure truffle uses a new provider every time.
-    // ropsten: {
-    //   provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    //   network_id: 3,       // Ropsten's id
-    //   gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    //   confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
-    //   timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    //   skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    goerli: {
+      provider: () => new HDWalletProvider(MNEMONIC, API_URL),
+      network_id: 5, // Ropsten's id
+      gas: 4000000, // Ropsten has a lower block limit than mainnet
+    },
     //
     // Useful for private networks
     // private: {
